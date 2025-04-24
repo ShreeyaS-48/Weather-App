74d04e56f5ca0a01e0bdca4e86efaf7a6bc28715
Shreeya Shah <shreeyashah@Shreeyas-MacBook-Pro.local>
2025-04-24 11:31:41 +0530

    test

--- serverless/get_weather.js
+++ serverless/get_weather.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const { WEATHER_API_KEY } = process.env;
 
-exports.handler = async (event, context) =>{
+exports.handler = async (event, context) => {
     const params = JSON.parse(event.body);
     const { lat, lon, units } = params;
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=7&units=${units}&appid=${WEATHER_API_KEY}`;
